0eae0a8cf647513bea9a53b576d6d05320e2bf8a
ChACiooh <y007com@naver.com>
2017-03-31 14:08:08 +0900

    solved2.

--- closureprac.playground/Contents.swift
+++ closureprac.playground/Contents.swift
@@ -14,46 +14,41 @@ print("\(sum)")
 var array2:[Double] = [10.0, 20.0, 50.0, 60.0, 200.0]
 
 let Vat:Double = 1.1
+let result = array2.map({$0 * Vat})
 
-for i in 0...4{
-    array2[i] *= Vat
-}
-
-print(array))
+print(result)
 
 
 
 //3번문제
-var array = [1,2,3,4,5]
+var array3 = [1,2,3,4,5]
+var result3:[Int] = array3.filter({$0<=3})
+let tmp:[Int] = array3.filter({$0>3}).map({$0*2})
+result3 += tmp
 
-for i in 0...4 {
-    if array[i] > 3 {
-        array[i] = array[i] * 2
-    }
-    
-}
-
-print(array)
+print(result3)
 
 
 //4번문제
 var dic = ["Alpha" : 3 , "Bravo" : 4 , "Charlie" : 0 , "Delta" : 2 , "Eco" : 0 , "Foxtrot":2]
-
-
-for (str,int) in dic {
-    if dic[str] == 0 {
-        dic[str] = dic[str]! + 1
+let result4 = dic.reduce([String:Int]()) { (before, now) -> [String:Int] in
+    var v = now.1
+    if v == 0 {
+        v += 1
     }
+    var result = before
+    result[now.0] = v
+    return result
 }
 
 
-dic
+print(result4)
 
 //5번문제
-var array  = [11 , 22 , 3 , 5 , 7]
+var array4  = [11 , 22 , 3 , 5 , 7]
 
 array.sorted{ (a:Int, b) -> Bool in
     return a < b
 }
 
-print("\(array.sorted()[0])")
+print("\(array4.sorted()[0])")
